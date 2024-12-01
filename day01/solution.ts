42eaa6b8a51de2406a0c4066c9386d46d21e66ce
radek.Bednarik <radek.bednarik@tesena.com>
2024-12-01 09:36:06 +0100

    _

--- day01/solution.ts
+++ day01/solution.ts
@@ -22,8 +22,35 @@ const input = raw
     return arr.sort();
   });
 
+// 1st part:
 input[1].forEach((num, i) => {
   result += Math.abs(num - input[0][i]);
 });
 
 console.log(result);
+
+// 2nd part:
+const computed = new Map();
+
+for (const num of input[0]) {
+  if (!computed.has(num)) {
+    let count = 0;
+
+    for (let n of input[1]) {
+      if (n === num) {
+        count++;
+      }
+    }
+
+    computed.set(num, num * count);
+  } else {
+    computed[num] += computed[num];
+  }
+}
+
+let resultTwo = 0;
+computed.forEach((value) => {
+  resultTwo += value;
+});
+
+console.log(resultTwo);
