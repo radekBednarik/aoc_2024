f174abe4f103544112c462eeb7120d3585db32d9
radekBednarik <radek.bednarik@stesena.com>
2024-12-02 08:52:32 +0100

    2nd: wip on second part

--- day02/solution.ts
+++ day02/solution.ts
@@ -50,36 +50,9 @@ for (const report of input) {
 console.log(safeReports);
 
 // part 2
+const safeReportsTwo = 0;
 for (const report of input) {
-	let flag = false;
-	let prevDiff = 0;
-
-	for (let i = 0; i < report.length - 1; i++) {
-		if (report[i] === report[i + 1]) {
-			flag = false;
-			break;
-		}
-
-		const diff = report[i] - report[i + 1];
-
-		if ((prevDiff > 0 && diff < 0) || (prevDiff < 0 && diff > 0)) {
-			flag = false;
-			break;
-		}
-
-		prevDiff = diff;
-
-		const absDiff = Math.abs(diff);
-
-		if (absDiff >= 1 && absDiff <= 3) {
-			flag = true;
-		} else {
-			flag = false;
-			break;
-		}
-	}
-
-	if (flag) {
-		safeReports++;
-	}
+	console.log("---");
 }
+
+console.log(safeReportsTwo);
