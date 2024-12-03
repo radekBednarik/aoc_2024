d3cf23d6271778dd9353f81b5506f4dcb8f1bfdb
radekBednarik <radek.bednarik@stesena.com>
2024-12-03 13:00:02 +0100

    chore: better restr for part I and II

--- day03/solution.ts
+++ day03/solution.ts
@@ -1,8 +1,11 @@
 import { readFile } from "node:fs/promises";
 
 const raw = await readFile("input.txt", { encoding: "utf-8" });
-const input = raw
-	.trim()
+const input = raw.trim();
+
+// part 1
+
+const resultOne = input
 	.match(/mul\(\d+,\d+\)/gm)
 	?.map((str) => {
 		return str.match(/\d+/g);
@@ -11,13 +14,10 @@ const input = raw
 		return arr?.map((val) => {
 			return Number.parseInt(val);
 		});
-	});
-
-// part 1
-
-const resultOne = input?.reduce((acc, nums) => {
-	acc += nums![0] * nums![1];
-	return acc;
-}, 0);
+	})
+	.reduce((acc, nums) => {
+		acc += nums![0] * nums![1];
+		return acc;
+	}, 0);
 
 console.log(resultOne);
