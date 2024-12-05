cf51526824e88b006f84042bce6196b3f3562883
radekBednarik <radek.bednarik@tesena.com>
2024-12-05 08:41:02 +0100

    day5: transformations

--- day05/solution.ts
+++ day05/solution.ts
@@ -5,8 +5,27 @@ const raw = await readFile("input.txt", { encoding: "utf-8" });
 const input = raw.trim().split("\n");
 const splitIndex = input.indexOf("");
 const rules = input.slice(0, splitIndex);
-const pagesNums = input.slice(splitIndex + 1);
+const pagesNums = input.slice(splitIndex + 1).map((row) => {
+  return row.split(",").map((item) => {
+    return parseInt(item);
+  });
+});
 
-console.log(JSON.stringify(rules));
-console.log("==========");
+const rulesMap = new Map();
+
+for (const rule of rules) {
+  const [key, val] = rule.split("|").map((val) => {
+    return parseInt(val);
+  });
+
+  if (rulesMap.has(key)) {
+    const currVal = rulesMap.get(key);
+    if (Array.isArray(currVal)) {
+      currVal.push(val);
+    }
+  } else {
+    rulesMap.set(key, [val]);
+  }
+}
+
 console.log(JSON.stringify(pagesNums));
