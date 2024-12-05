635c3c889185606efeae00af1289a5e33114599d
radekBednarik <radek.bednarik@tesena.com>
2024-12-05 07:20:11 +0100

    day5: parse input

--- day05/solution.ts
+++ day05/solution.ts
@@ -1,3 +1,12 @@
 import { readFile } from "node:fs/promises";
 
 const raw = await readFile("input.txt", { encoding: "utf-8" });
+
+const input = raw.trim().split("\n");
+const splitIndex = input.indexOf("");
+const rules = input.slice(0, splitIndex);
+const pagesNums = input.slice(splitIndex + 1);
+
+console.log(JSON.stringify(rules));
+console.log("==========");
+console.log(JSON.stringify(pagesNums));
