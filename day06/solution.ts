c40c5302fed47a0f87721f7a81c8402a92e5ce30
radekBednarik <radek.bednarik@tesena.com>
2024-12-06 15:28:01 +0100

    day6: wip

--- day06/solution.ts
+++ day06/solution.ts
@@ -22,13 +22,18 @@ const startDir = directions[grid[startPos![0]][startPos![1]]];
 positions.add(startPos);
 
 function simulate() {
-	//
+	const pos = startPos;
+	const dir = startDir;
+
+	while (true) {
+    if (isOutOfBounds(pos, grid))
+  }
 }
 
 function findStartingPosition(
 	grid: string[][],
 	directions: Record<string, string>,
-) {
+) : [number, number]{
 	for (let i = 0; i < grid.length; i++) {
 		for (let j = 0; j < grid[i].length; j++) {
 			if (Object.values(directions).includes(grid[i][j])) {
@@ -36,4 +41,14 @@ function findStartingPosition(
 			}
 		}
 	}
+
+  throw new Error("Did not found starting point")
 }
+
+function isOutOfBounds(position: [number, number], grid: string[][]) {
+	const [x, y] = position;
+
+	return x < 0 || x >= grid[0].length || y < 0 || y >= grid.length;
+}
+
+console.log(startDir, startPos);
