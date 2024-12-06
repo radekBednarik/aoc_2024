f50204e2a1eb8d564ac8ee3f45a7d202d679df94
radekBednarik <radek.bednarik@tesena.com>
2024-12-06 08:46:25 +0100

    day6: get starting position

--- day06/solution.ts
+++ day06/solution.ts
@@ -8,4 +8,28 @@ const grid = raw
 		return row.split("");
 	});
 
-console.log(grid);
+const directions = new Map();
+directions.set("right", ">");
+directions.set("down", "\u25BC");
+directions.set("left", "<");
+directions.set("up", "^");
+
+const positions = new Set();
+positions.add(findStartingPosition(grid, directions));
+
+function findStartingPosition(
+	grid: string[][],
+	directions: Map<string, string>,
+) {
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			for (const item of directions.values()) {
+				if (item === grid[i][j]) {
+					return [i, j];
+				}
+			}
+		}
+	}
+}
+
+console.log(positions);
