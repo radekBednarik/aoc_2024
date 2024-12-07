7aff4759b7b661bc6390f48984d2ce2076dcf2c2
radekBednarik <radek.bednarik@stesena.com>
2024-12-07 08:20:57 +0100

    day7: part one done

--- day07/solution.ts
+++ day07/solution.ts
@@ -21,6 +21,58 @@ const input = raw_2.map((row) => {
 				return Number.parseInt(val);
 			});
 	});
-});
+}) as [number, number[]][];
 
-console.log(JSON.stringify(input));
+// part one:
+
+let partOneResult = 0;
+
+for (const row of input) {
+	const [result, numbers] = row;
+	const combinations = generateCombinations(numbers);
+
+	for (const combination of combinations) {
+		let tempResult = numbers[0];
+
+		for (let i = 0; i < combination.length; i++) {
+			const op = combination[i];
+			const number = numbers[i + 1];
+
+			if (op === "*") {
+				tempResult *= number;
+			} else {
+				tempResult += number;
+			}
+		}
+
+		if (result === tempResult) {
+			partOneResult += result;
+			break;
+		}
+	}
+}
+
+console.log(partOneResult);
+
+//------ helper functions
+
+function generateCombinations(numbers: number[]): string[][] {
+	const operators = ["*", "+"];
+	const results: string[][] = [];
+
+	function helper(current: string[], index: number) {
+		if (index === numbers.length - 1) {
+			results.push([...current]);
+			return;
+		}
+
+		for (const operator of operators) {
+			current.push(operator);
+			helper(current, index + 1);
+			current.pop();
+		}
+	}
+
+	helper([], 0);
+	return results;
+}
