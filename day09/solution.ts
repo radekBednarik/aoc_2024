f0fa5bbfaa5a7a506c2df14ab3b22ef3f8c2db9e
radekBednarik <radek.bednarik@stesena.com>
2024-12-09 07:52:32 +0100

    day9: represent disk as array of chars

--- day09/solution.ts
+++ day09/solution.ts
@@ -10,6 +10,12 @@ const input = raw
 
 const initDiskMap = mapDisk(input);
 
+console.log(initDiskMap);
+
+const diskArray = createDiskArr(initDiskMap);
+
+console.log(diskArray);
+
 // helper functions
 
 function mapDisk(
@@ -17,8 +23,8 @@ function mapDisk(
 ): { type: "file" | "empty"; length: number }[] {
 	const map: { type: "file" | "empty"; length: number }[] = [];
 
-	input.forEach((val) => {
-		if (val % 2 === 0) {
+	input.forEach((val, i) => {
+		if (i % 2 === 0) {
 			map.push({ type: "file", length: val });
 		} else {
 			map.push({ type: "empty", length: val });
@@ -28,4 +34,22 @@ function mapDisk(
 	return map;
 }
 
-console.log(initDiskMap[0], initDiskMap[1]);
+function createDiskArr(diskMap: { type: "file" | "empty"; length: number }[]) {
+	const diskArr: string[] = [];
+	let fileIndex = 0;
+
+	diskMap.forEach((obj) => {
+		if (obj.length === 0) {
+			diskArr.push("");
+		}
+
+		if (obj.type === "file") {
+			diskArr.push(`${fileIndex}`.repeat(obj.length));
+			fileIndex++;
+		} else {
+			diskArr.push(`.`.repeat(obj.length));
+		}
+	});
+
+	return diskArr.join("");
+}
