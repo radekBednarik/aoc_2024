686af72b0fda2b97d6f34d5dc351f6f166256a31
radekBednarik <radek.bednarik@stesena.com>
2024-12-09 07:11:45 +0100

    day9: init disk map

--- day09/solution.ts
+++ day09/solution.ts
@@ -1,4 +1,29 @@
 import { readFile } from "fs/promises";
 
 const raw = await readFile("input.txt", { encoding: "utf-8" });
-const input = raw.trim();
+const input = raw
+	.trim()
+	.split("")
+	.map((val) => {
+		return Number.parseInt(val, 10);
+	});
+
+const initDiskMap = mapDisk(input);
+
+// helper functions
+
+function mapDisk(input: number[]) {
+	const map: { type: "file" | "empty"; length: number }[] = [];
+
+	input.forEach((val) => {
+		if (val % 2 === 0) {
+			map.push({ type: "file", length: val });
+		} else {
+			map.push({ type: "empty", length: val });
+		}
+	});
+
+	return map;
+}
+
+console.log(initDiskMap[0], initDiskMap[1]);
