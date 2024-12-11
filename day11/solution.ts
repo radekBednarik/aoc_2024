246572464b6d060a69d710347e8a5cfbbfce60f0
radekBednarik <radek.bednarik@stesena.com>
2024-12-11 10:37:34 +0100

    day11: _

--- day11/solution.ts
+++ day11/solution.ts
@@ -8,16 +8,17 @@ const input = raw
 		return Number.parseInt(val);
 	});
 
-let arr: number[] = [];
+let arr: number[] = input;
+
+console.time("Execution time");
 
 for (let i = 0; i < 25; i++) {
-	const temp = i === 0 ? input : arr;
 	const tempArr: number[] = [];
 
-	for (let j = 0; j < temp.length; j++) {
-		const numAsString = temp[j].toString();
+	for (let j = 0; j < arr.length; j++) {
+		const numAsString = arr[j].toString();
 
-		if (temp[j] === 0) {
+		if (arr[j] === 0) {
 			tempArr.push(1);
 		} else if (numAsString.length % 2 === 0) {
 			const mid = Math.floor(numAsString.length / 2);
@@ -25,11 +26,13 @@ for (let i = 0; i < 25; i++) {
 			tempArr.push(Number.parseInt(numAsString.slice(0, mid)));
 			tempArr.push(Number.parseInt(numAsString.slice(mid)));
 		} else {
-			tempArr.push(temp[j] * 2024);
+			tempArr.push(arr[j] * 2024);
 		}
 	}
 
 	arr = tempArr;
 }
+
+console.timeEnd("Execution time");
 
 console.log("First part: ", arr.length);
